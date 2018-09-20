f626b5db6a3a4e885f6512095007f014e9bad547
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-19 17:39:25 -0700

    Made working customer class

--- customers.py
+++ customers.py
@@ -1,7 +1,41 @@
 """Customers at Hackbright."""
 
 
-class Customer(object):
+class Customer:
     """Ubermelon customer."""
 
-    # TODO: need to implement this
+    def __init__(self,first_name, last_name, email, password):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
+
+    def __repr__(self):
+        """Convenience method to show info about customer in console."""
+
+        return "<Customer: {}, {}, {}>".format(self.first_name, self.last_name, self.email)
+
+def read_customers_from_file(filepath):
+    """Read customer file and populate customer dictionary. 
+
+    Returned dictionary will be {id: Customer object}
+    """
+
+    customers = {}
+
+    file = open(filepath)
+
+    for line in file:
+        (first_name, last_name, email, password) = line.strip().split("|")
+
+        customers[email] = Customer(first_name, last_name, email, password)
+
+    return customers
+
+
+def get_by_email(email):
+    
+
+    return customers[email]
+
+customers = read_customers_from_file("customers.txt")
